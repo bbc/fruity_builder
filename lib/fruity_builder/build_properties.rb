70311bf607ed423a892f5880da2969d89a88f9b5
Jon Wilson <jon.wilson01@bbc.co.uk>
2015-09-14 11:54:26 +0100

    Initial commit

---
+++ lib/fruity_builder/build_properties.rb
@@ -0,0 +1,70 @@
+require 'fruity_builder/lib/execution'
+require 'fruity_builder/plistutil'
+
+module FruityBuilder
+  module IOS
+    class BuildProperties < Execution
+
+      attr_accessor :project, :properties
+
+      def initialize(project_path)
+        @project = project_path
+      end
+
+      def open_project_properties
+        @properties = File.read(@project)
+      end
+
+      def properties
+        open_project_properties if @properties.nil?
+        @properties
+      end
+
+      def save_project_properties
+        File.write(@project, properties)
+      end
+
+      def replace_bundle_id(new_bundle_id)
+        path = Pathname.new(File.dirname(@project) + '/../').realdirpath.to_s
+        xcode = FruityBuilder::IOS::XCodeBuild.new(File.dirname(@project))
+        targets = xcode.get_targets
+        project_files = Dir["#{path}/**/Info.plist"]
+
+        files = project_files.select { |project| targets.any? { |target| project.include?("#{target}/Info.plist") } }
+        files.each do |file|
+          FruityBuilder::IOS::Plistutil.replace_bundle_id(new_id: new_bundle_id, file: file)
+        end
+      end
+
+      def replace_dev_team(new_dev_team)
+        @properties = self.class.replace_project_data(regex: '.*DevelopmentTeam = (.*);.*', data: properties, new_value: new_dev_team)
+      end
+
+      def replace_code_sign_identity(new_identity)
+        @properties = self.class.replace_project_data(regex: '.*CODE_SIGN_IDENTITY.*= "(.*)";.*', data: properties, new_value: new_identity)
+      end
+
+      def replace_provisioning_profile(new_profile)
+        @properties = self.class.replace_project_data(regex: '.*PROVISIONING_PROFILE = "(.*)";.*', data: properties, new_value: new_profile)
+      end
+
+      def self.replace_project_data(options = {})
+        regex = Regexp.new(options[:regex])
+        replacements = options[:data].scan(regex).uniq.flatten
+
+        result = options[:data]
+        replacements.each do |to_replace|
+          result = result.gsub(to_replace, options[:new_value])
+        end
+
+        result
+      end
+    end
+
+    class BuildPropertiesError < StandardError
+      def initialize(msg)
+        super(msg)
+      end
+    end
+  end
+end
---
+++ lib/fruity_builder/lib/sys_log.rb
@@ -0,0 +1,39 @@
+require 'syslog'
+
+module FruityBuilder
+  class SysLog
+
+    attr_accessor :syslog
+
+    def log(priority,message)
+      if @syslog and @syslog.opened?
+        @syslog = Syslog.reopen('device-api-gem', Syslog::LOG_PID, Syslog::LOG_DAEMON)
+      else
+        @syslog = Syslog.open('device-api-gem', Syslog::LOG_PID, Syslog::LOG_DAEMON)
+      end
+
+      @syslog.log(priority,message)
+      @syslog.close
+    end
+
+    def fatal(message)
+      self.log(Syslog::LOG_CRIT,message)
+    end
+
+    def error(message)
+      self.log(Syslog::LOG_ERR,message)
+    end
+
+    def warn(message)
+      self.log(Syslog::LOG_WARNING,message)
+    end
+
+    def info(message)
+      self.log(Syslog::LOG_INFO,message)
+    end
+
+    def debug(message)
+      self.log(Syslog::LOG_DEBUG,message)
+    end
+  end
+end
