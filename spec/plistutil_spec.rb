807f280482bd83fee874e38c270526898b2f2914
Jon Wilson <jon.wilson01@bbc.co.uk>
2016-05-05 10:18:59 +0100

    Moved the plistutil and signing classes into the IOS namespace as they are platform specific Bumped minor version

--- spec/plistutil_spec.rb
+++ spec/plistutil_spec.rb
@@ -1,15 +1,15 @@
 require 'fruity_builder/plistutil'
 
-describe FruityBuilder::Plistutil do
+describe FruityBuilder::IOS::Plistutil do
   describe 'Bundle ID -' do
     it 'should get the bundle ID' do
-      bundle_ids = FruityBuilder::Plistutil.get_bundle_id(xml: File.read("#{Dir.pwd}/spec/project_files/info.plist"))
+      bundle_ids = FruityBuilder::IOS::Plistutil.get_bundle_id(xml: File.read("#{Dir.pwd}/spec/project_files/info.plist"))
       expect(bundle_ids).to eq(["uk.co.bbc.testapp", "$(PRODUCT_NAME)"])
     end
 
     it 'should replace the bundle ID' do
-      new_plist = FruityBuilder::Plistutil.replace_bundle_id(xml: File.read("#{Dir.pwd}/spec/project_files/info.plist"), new_id: 'uk.co.bbc.test')
-      expect(FruityBuilder::Plistutil.get_bundle_id(xml: new_plist)).to eq(["uk.co.bbc.test"])
+      new_plist = FruityBuilder::IOS::Plistutil.replace_bundle_id(xml: File.read("#{Dir.pwd}/spec/project_files/info.plist"), new_id: 'uk.co.bbc.test')
+      expect(FruityBuilder::IOS::Plistutil.get_bundle_id(xml: new_plist)).to eq(["uk.co.bbc.test"])
     end
   end
 end
